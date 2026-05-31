8ca35a092f89a66fc3a2605ae7042be377c65280
Sahil Thakkar <thakkar.sahil88@gmail.com>
2026-05-31 20:48:55 +0530

    fix: middleware authorized callback for login loop

--- middleware.ts
+++ middleware.ts
@@ -1,6 +1,11 @@
 import { withAuth } from 'next-auth/middleware'
 
-export default withAuth({ pages: { signIn: '/' } })
+export default withAuth({
+  pages: { signIn: '/' },
+  callbacks: {
+    authorized: ({ token }) => !!token,
+  },
+})
 
 export const config = {
   matcher: [
